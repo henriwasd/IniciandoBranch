50f58aee54771a5cd091b57740f940eac5b5e025
henrique silva <henrique.rrs@hotmail.com>
2019-10-10 17:18:13 -0300

    Cleitones monstro

---
+++ SimpleApp/LibrarySimple/Controller/UserController.cs
@@ -0,0 +1,38 @@
+﻿using LibrarySimple.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySimple.Controller
+{
+    public class UserController
+    {
+        UserContextDB contextDB = new UserContextDB();
+
+        /// <summary>
+        /// Metodo que lista os usuarios
+        /// </summary>
+        /// <returns>Retorna uma lista de usuarios</returns>
+        public IQueryable<User> GetUsers()
+        {
+            return contextDB.Users.Where(x => x.Ativo == true);
+        }
+
+        /// <summary>
+        /// Metodo para adicionar nomes
+        /// </summary>
+        /// <param name="user">Entrada do usuario</param>
+        /// <returns>Retorna se foi adiciona com sucesso ou não</returns>
+        public bool AddUser(User user)
+        {
+            if (string.IsNullOrWhiteSpace(user.Name))
+                return false;
+
+            contextDB.Users.Add(user);
+            contextDB.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ SimpleApp/LibrarySimple/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySimple.Model
+{
+    public class User : ControlUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
